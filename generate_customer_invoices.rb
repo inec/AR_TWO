8d76908f989071b9e147a8fd09002e56e1ef6c01
Yenchi Lu <ylu42@academic.rrc.ca>
2013-02-11 00:13:40 -0600

    almost finished

---
+++ generate_customer_invoices.rb
@@ -0,0 +1,59 @@
+# The ar.rb file loads all of our ActiveRecord Object and
+# establishes our database connection.
+load 'ar.rb'
+
+def currency amount
+ sprintf("$%.2f",amount)
+end
+customers_with_orders = Customer.includes('orders').where('orders.id IS NOT NULL').where('orders.status = "new"')
+
+#puts customers_with_orders[1].inspect
+#customer=customers_with_orders[1]
+customers_with_orders.each do |customer|
+#puts customer.inspect
+#puts customer.orders.count
+#new_order=customer.orders[0]
+#puts new_order.inspect 
+#puts new_order.line_items
+customer.orders.each do |new_order|
+
+  #show customer name and address
+  puts "Invoice for #{customer.first_name} #{customer.last_name}"
+  puts "#{customer.address}"
+  puts "#{customer.city} , #{customer.province.name}"
+
+  puts""
+
+  sub_total=0
+  new_order.line_items.each do |line_item|
+
+    item_name=   line_item.product.name
+    item_price=  line_item.price
+    item_quantity = line_item.quantity
+    item_total=   item_quantity*item_price
+    #show name, quantity, price and calulated total for this item
+    puts "#{item_name} ... #{item_quantity} x #{currency(item_price)} = #{currency(item_total)}"
+    sub_total+=item_total #add to sub total
+  end
+
+
+  pst= (sub_total*new_order.pst_rate).round(2)
+  gst=(sub_total*new_order.gst_rate).round(2)
+  hst=(sub_total*new_order.hst_rate).round(2)
+  grand_total=sub_total+pst+gst+hst
+
+  # #show total result
+   puts ""
+   puts "Sub Total     :  #{currency(sub_total)}"
+   
+
+
+   puts "PST (#{new_order.pst_rate.round(2)*100}%)    :  #{currency(pst)}" unless pst.zero?
+   puts "GST (#{new_order.gst_rate.round(2)*100}%)    :  #{currency(gst)}" unless gst.zero?
+   puts "HST (#{new_order.hst_rate.round(2)*100}%)   :  #{currency(hst)}" unless hst.zero?
+
+   puts "Grand Total   :  #{currency(grand_total)}"
+   puts "\n"
+
+  end
+ end
---
+++ seed_customer_orders.rb
@@ -0,0 +1,52 @@
+# The ar.rb file loads all of our ActiveRecord Object and
+# establishes our database connection.
+load 'ar.rb'
+
+# Create and persist orders for three customers. (Each customer should be from a different province.)
+customers_queue=[]
+(1..3).each do |province_id|  
+customers_queue<<Customer.where(:province_id => province_id).first
+end
+
+# add one existed customer in queue for second order
+customers_queue<<customers_queue[rand(0..customers_queue.size)]
+
+# Each order should involve at least four line items. (Use different products for each customer.)
+product_array=Product.limit(20).to_a
+
+customers_queue.each do |cust|
+  new_order=cust.orders.build
+  # Ensure that you set the status of all orders to ‘new’
+  new_order.status="new"
+
+
+  # Ensure that you backup the pst, gst and hst rates from the customer’s province into the appropriate properties of the order.
+  new_order.gst_rate = cust.province.gst
+  new_order.pst_rate= cust.province.pst
+  new_order.hst_rate=cust.province.hst
+  #puts new_order.inspect
+
+  new_order.save
+
+
+  #Each order should involve at least four line items.
+  4.times do
+    product_index=rand(0..product_array.count)
+    # Ensure that each line item is associated with an order by way of its foreign key.
+    new_lineitem=new_order.line_items.build
+    # Ensure that each line item has a set quantity.
+    new_lineitem.quantity=rand(1..20)
+    product_index=rand(product_array.size)
+
+  
+    current_product=product_array[product_index]
+    # Ensure that each line item is associated with a specific product by way of its foreign key.
+    new_lineitem.product = current_product
+    # Ensure that a backup of the associated product price is saved in each line item.
+    new_lineitem.price=current_product.price
+    product_array.delete_at(product_index)
+
+    #puts new_lineitem.inspect
+    new_lineitem.save
+  end
+end
